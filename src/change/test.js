83baa77d6fc54d66150ab2b6bd53cac6d3e9aa8a
keyserfaty <keserfaty@gmail.com>
2019-02-08 09:19:14 -0300

    refactor tests

--- src/change/test.js
+++ src/change/test.js
@@ -2,20 +2,22 @@ import React from "react";
 import { create } from "react-test-renderer";
 import Change from ".";
 
-it("changes the value when typing on the input", () => {
+describe('tests change', () => {
   const component = create(<Change />);
   const rootInstance = component.root;
 
   const input = rootInstance.findByType("input");
   const container = rootInstance.findByType("div");
 
-  let tree = component.toJSON()
-  expect(tree).toMatchSnapshot()
-  
-  input.props.onChange({ target: { name: 'item', value: 'some' } });
-  
-  tree = component.toJSON()
-  expect(tree).toMatchSnapshot()
-  
-  expect(container.props.children).toContain("some");
-});
+  it("changes the value when typing on the input", () => {
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+    
+    input.props.onChange({ target: { name: 'item', value: 'some' } });
+    
+    tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+    
+    expect(container.props.children).toContain("some");
+  });
+})
